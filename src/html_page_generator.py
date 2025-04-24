b6ffd2587aa4ce30cd35dcef92f444c4b70812f9
rahul-mallah <mallahrahul@gmail.com>
2025-04-24 15:07:58 +0800

    Chnages for deploy to github pages

--- src/html_page_generator.py
+++ src/html_page_generator.py
@@ -52,26 +52,35 @@ def write_contents(file_path, contents):
         f.write(contents)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    # read md and template files
     markdown_contents = get_file_contents(from_path)
     template_contents = get_file_contents(template_path)
+    # convert md to html
     html_contents = markdown_to_html_node(markdown_contents).to_html()
+    # extract h1 title
     title = extract_title(markdown_contents)
+    # replace title and content to template contents
     template_contents = template_contents.replace("{{ Title }}", title)
-    full_html = template_contents.replace("{{ Content }}", html_contents)
-    write_contents(dest_path, full_html)
+    template_contents = template_contents.replace("{{ Content }}", html_contents)
+    # replace basepath in hyperlinks and images
+    template_contents = template_contents.replace("href=\"/", f"href=\"{basepath}")
+    template_contents = template_contents.replace("src=\"/", f"src=\"{basepath}")
+    # create file and write contents
+    write_contents(dest_path, template_contents)
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for name in os.listdir(dir_path_content):
         src_path = os.path.join(dir_path_content, name)
         # check if it is md file then generate html page
         if os.path.isfile(src_path):
             if name.endswith(".md"):
                 dest_path = os.path.join(dest_dir_path, name.replace(".md", ".html"))
-                generate_page(src_path, template_path, dest_path) 
+                generate_page(src_path, template_path, dest_path, basepath) 
         else:
             # else create directory in dest
             dest_path = os.path.join(dest_dir_path, name)
             os.mkdir(dest_path)
-            generate_page_recursive(src_path, template_path, dest_path)
+            generate_page_recursive(src_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
+import sys
 from textnode import *
 from html_page_generator import *
 
 
 def main():
-    move_files("static", "public")
-    generate_page_recursive("content", "template.html", "public")
+    basepath = "/"
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    elif len(sys.argv) > 2:
+        print(f"Usage: ./build.sh <basepath>")
+        sys.exit(1)
+    
+    move_files("static", "docs")
+    generate_page_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
